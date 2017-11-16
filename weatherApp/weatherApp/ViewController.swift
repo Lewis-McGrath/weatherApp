b359431000732f5fc104d3d93f3ddfd16626be2a
Lewis McGrath <lewismcgrath7@gmail.com>
2017-11-16 15:36:53 +0000

    Added additional struct for response. Cleaned up Model Objects.

--- weatherApp/weatherApp/ViewController.swift
+++ weatherApp/weatherApp/ViewController.swift
@@ -13,19 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let myData = JSON()
-        
-        let decoder = JSONDecoder()
-        do {
-            
-            let model = try decoder.decode(WeatherResponse2.self, from: myData.data)
-            print(model.list.first ?? "nothing to print")
-            
-        } catch {
-            print(error)
-        }
+       
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
